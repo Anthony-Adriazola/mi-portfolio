03309f0dad2c65743636dd4137b2909c2b748fa7
Anthony <diego.adriazola@uab.edu.com>
2026-03-01 18:08:53 -0400

    chore: finalize project image and config; remove debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,8 @@ import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
-const isProd = process.env.NODE_ENV === 'production'
-
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://anthony-adriazola.github.io',
-  base: isProd ? '/mi-portfolio' : undefined,
+  base: process.env.CI ? '/mi-portfolio' : '/',
 })
